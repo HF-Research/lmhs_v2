c38dcbf59f684019a355e4a6f40bf92475296feb
matthew-phelps <ma.phelps@gmail.com>
2021-02-10 10:16:20 +0100

    Initial commit

---
+++ global.R
@@ -0,0 +1,29 @@
+# LIBRARIES ----------------------------------------------------
+# devtools::install_github("rstudio/profvis")
+# library(profvis)
+# remotes::install_github("rstudio/reactlog")
+library(reactlog)
+# reactlog_enable()
+# devtools::install_version("shiny", version = "1.5.0")
+suppressPackageStartupMessages({
+  library(shiny)
+  library(DT)
+  library(shinyWidgets)
+  library(data.table)
+  library(shinyjs) # Hides un-used tabs
+  library(manipulateWidget)
+  library(sf)
+  # devtools::install_github("ropensci/plotly")
+  library(plotly)
+  library(leaflet)
+  library(fst)
+  library(magrittr)
+})
+
+# LOAD R CODE -------------------------------------------------------------
+# This should be done automatically in shiny versions >= 1.5
+# However, on deployment to shinyapps, it seems these codes are note loaded
+files <- list.files(path = "R/", full.names = TRUE)
+sapply(files, source)
+
+enableBookmarking("url")
