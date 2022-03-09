ec8548583f3e75a93cb102fb8f1816bf371914e5
matthew-phelps <ma.phelps@gmail.com>
2022-03-09 09:04:59 +0100

    Update results with new stratifications

--- init/deploy.R
+++ init/deploy.R
@@ -1,2 +1,2 @@
 # DO NOT RUN THIS SCRIPT DIRECTLY, SOURCE THE CALL_DEPLOY_LOCAL SCRIPT
-rsconnect::deployApp(appName = 'LMHS', forceUpdate = TRUE)
+rsconnect::deployApp(appName = 'LMHS-dev', forceUpdate = TRUE)
